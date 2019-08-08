703b6a9b381e5087d239b03f2680c04aedb05ff1
luxargon <luiscamurillo@gmail.com>
2019-08-08 17:03:26 -0600

    1 file modified, 1 added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # angular-6ircc7
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6ircc7)
+
+
+
---
+++ src/app/role.ts
@@ -0,0 +1,34 @@
+
+export const roles = [
+  {
+    name: 'SAP',
+    description: 'SAP technologies',
+    area: 'area test',
+    unit: 'unit test'
+  },
+
+  {
+    name: 'Development',
+    description: 'A large phone with one of the best screens',
+    area: 'area test',
+    unit: 'unit test'
+  },
+
+  {
+    name: 'Linux',
+    description: 'A large phone with one of the best screens',
+    area: 'area test',
+    unit: 'unit test'
+  },
+  
+    {
+    name: 'DB2',
+    description: 'A large phone with one of the best screens',
+    area: 'area test',
+    unit: 'unit test'
+  },
+
+];
+
+
+
